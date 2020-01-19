b7c4a917169bd32e413796e113401be266d9f4dd
yuga77 <y-n7c8@i.softbank.jp>
2020-01-19 23:25:53 +0900

    update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,56 @@
 $(function(){
-  function bulidHTML(message){
-    if ( message.image ){
-      var html = 
-        `<div class="message" data-message-id=${message.id}>
-          <div class="message__upper-info">
-            <div class="message__upper-info__name">
-              ${message.user_name}
-            </div>
-            <div class="message__upper-info__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="message__text">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-          <img src=${message.image} >
-        </div>`
-      return html;
-    } else {
-      var html =
-        `<div class="message" data-message-id=${message.id}>
-            <div class="message__upper-info">
-              <div class="message__upper-info__name">
-                ${message.user_name}
-             </div>
-             <div class="message__upper-info__date">
-               ${message.created_at}
-             </div>
-            </div>
-            <div class="message__text">
-             <p class="lower-message__content">
-               ${message.content}
-             </p>
-           </div>
-         </div>`
-      return html;
+
+  var buildHTML = function(message) {
+    if (message.content && message.image) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="message__upper-info">` +
+          `<div class="message__upper-info__name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="message__upper-info__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message__text">` +
+          `<p class="lower-message__content">` +
+            message.content +
+          `</p>` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    } else if (message.content) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="message__upper-info">` +
+          `<div class="message__upper-info__name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="message__upper-info__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message__text">` +
+          `<p class="lower-message__content">` +
+            message.content +
+          `</p>` +
+        `</div>` +
+      `</div>`
+    } else if (message.image) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="message__upper-info">` +
+          `<div class="message__upper-info__name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="message__upper-info__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message__text">` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
     };
-  }
+    return html;
+  };
   $('#new_message').on('submit', function(e){
     e.preventDefault()
     var formData =new FormData(this);
@@ -52,7 +64,7 @@ $(function(){
       contentType: false
     })
     .done(function(data){
-      var html = bulidHTML(data);
+      var html = buildHTML(data);
       $('.messages').append(html);
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
       $('form')[0].reset();
@@ -62,4 +74,32 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   })
+
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $("#new_message")[0].reset();
+        $(".form__submit").prop("disabled", false);
+      }
+    })
+    .fail(function() {
+      console.log('error')
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,8 +8,8 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    if @message.save
-      respond_to do |format|
+      if @message.save
+        respond_to do |format|
         format.json
       end
     else
