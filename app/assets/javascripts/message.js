73887cd84c697032265882c8dfd7d1dd19d6278f
yuga77 <y-n7c8@i.softbank.jp>
2020-01-20 10:17:15 +0900

    change alert

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -96,7 +96,7 @@ $(function(){
       }
     })
     .fail(function() {
-      console.log('error')
+      alert('自動更新に失敗しました')
     });
   };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
