d01e3b32190a1d8ca002d8d84eb6f90bc80a9909
yuga77 <y-n7c8@i.softbank.jp>
2020-01-20 21:28:50 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' 
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/chat-space.pem'] 
+                  keys: ['~/.ssh/ChatSpace.pem'] 
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
